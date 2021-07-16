66613827a1c44a84a0e75aff4c5cbc200b5bc9ea
Deepanshu <deepanshuchhanna@gmail.com>
2021-07-16 19:05:25 +0530

    resolve errors

--- projects/chatbook/src/App.js
+++ projects/chatbook/src/App.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/react-in-jsx-scope */
+// eslint-disable-next-line no-unused-vars
 import react from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
--- projects/chatbook/src/index.js
+++ projects/chatbook/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable spaced-comment */
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
@@ -6,11 +7,7 @@ import App from './App';
 
 //import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
