99dc254985a99820833e6f549464203350d71c31
Deepanshu <deepanshuchhanna@gmail.com>
2021-07-20 14:10:54 +0530

    created public and private Route

--- projects/chatbook/src/App.js
+++ projects/chatbook/src/App.js
@@ -4,9 +4,24 @@
 import react from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import { Switch } from 'react-router';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+
+import Home from './pages/Home';
 
 function App() {
-  return <div>CHAT BOOK</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signIn">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
