e92dde1b33b52723aca7ae82fe003c43bdef1a98
Deepanshu <deepanshuchhanna@gmail.com>
2021-07-21 17:33:46 +0530

    created a responsive dashboard for mobile users

--- projects/chatbook/src/components/dashboard/DashboardToggle.js
+++ projects/chatbook/src/components/dashboard/DashboardToggle.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Button, Drawer, Icon } from 'rsuite';
 import Dashboard from '.';
-import { useModalState } from '../../misc/custom-hooks';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
+  // for desktop it remains same but as we use this in mobile or we reduce the screen size by using cursor it will call media query which make it responsive. which we have created in custom-hooks.js file in misc folder
+  const isMobile = useMediaQuery('(max-width:992px)');
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" />
         DashBoard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
