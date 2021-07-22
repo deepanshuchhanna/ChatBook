12c2829d8683599a8ff496ca8de66f7b2772b638
Deepanshu <deepanshuchhanna@gmail.com>
2021-07-22 14:00:08 +0530

    created resusable editable Input

--- projects/chatbook/src/components/dashboard/index.js
+++ projects/chatbook/src/components/dashboard/index.js
@@ -1,12 +1,18 @@
 /* eslint-disable import/order */
 /* eslint-disable import/no-duplicates */
 import React from 'react';
-import { Drawer } from 'rsuite';
+import { Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import { Button } from 'rsuite';
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
+
+  const onSave = async newData => {
+    // eslint-disable-next-line no-console
+    console.log(newData);
+  };
   return (
     <>
       <Drawer.Header>
@@ -15,6 +21,13 @@ const Dashboard = ({ onSignOut }) => {
 
       <Drawer.Body>
         <h3> Hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
