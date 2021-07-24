4235ca9b97b8dbc6418f521bda39f66ede7234ee
Deepanshu <deepanshuchhanna@gmail.com>
2021-07-24 21:58:29 +0530

    started work on rooms components and chatRoom

--- projects/chatbook/src/pages/Home.js
+++ projects/chatbook/src/pages/Home.js
@@ -5,8 +5,9 @@ import Sidebar from '../components/Sidebar';
 const Home = () => {
   return (
     <Grid fluid className="h-100">
-      <Row>
-        <Col xs={24} md={8}>
+      {/* here we provide Row and Col 's height as h-100 to make the height 100 for nested components  */}
+      <Row className="h-100">
+        <Col xs={24} md={8} className="h-100">
           <Sidebar />
         </Col>
       </Row>
